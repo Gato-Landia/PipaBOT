c3fb6a1092c4d0eb02b3ac4b302aab2bc70ca201
KitKat <leonel.-s@hotmail.com>
2019-12-11 23:45:00 -0300

    Update PipaBOT 1.1.2

---
+++ Essentials/commands/help.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'help',
+    aliases: ['commands', 'command-list', 'commands-list'],
+    description: 'list of available commands',
+    execute(message) {
+      const embed = new Discord.RichEmbed()
+        .setThumbnail("https://cdn.discordapp.com/attachments/474331584447643658/647640189379936276/Sin_titulo.png")
+        .setColor("#74FF00")
+        .setTitle(`Información sobre el bot:`)
+        .addField("Programadores:", "Blox, GatoLandia")
+        .addField("Versión del bot:", "1.1")
+        .addField("Repositorio (Actualmente solo en GitHub):", "https://github.com/Gato-Landia/PipaBOT")
+        .addField("pp!pipa", "Muestra una imagen de un loro aleatorio")
+        .addField("pp!hola", "Pipa te saluda")
+        .addField("pp!piedra/pp!papel/pp!tijeras", "Inicias una ronda de Piedra, papel o tijeras contra Pipa")
+        .addField("pp!siono", "Di una pregunta de si o no, Pipa te la responderá")
+        .addField("pp!invite", "Pipa enviará un link para invitar al bot a cualquier servidor")
+        .addField("pp!ping", "Mira tu ping (ms)!")
+        .addField("pp!kick [Nombre] [Razón]", "Kickea a una persona del servidor")
+        .addField("pp!ban [Nombre] [Razón]", "Banea a una persona del servidor")
+        .addField("pp!borrar [Número de mensajes]", "Borra un determinado número de mensajes en el canal")
+        .addField("pp!serverinfo", "Muestra información del servidor")
+        .addField("pp!avatar [Nombre]", "Muestra el avatar de la persona elegida, si no se especifica se muestra tu avatar")
+        .addField("pp!repite", "Pipa repitirá lo que digas");
+        message.channel.send(embed);
+    },
+};
