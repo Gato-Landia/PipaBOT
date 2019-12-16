78a22f3b11fe3229db9d47b498ea6c019622a03e
KitKat <leonel.-s@hotmail.com>
2019-12-15 22:47:27 -0300

    Update PipaBOT 1.1.4

--- Essentials/commands/searchgl.js
+++ Essentials/commands/searchgl.js
@@ -4,10 +4,11 @@ module.exports = {
     aliases: ['search-gl', 'search-google', 'searchgoogle'],
     execute: async (message, args) => {
     let searchQTitle = args.join(" ");
-    let searchQ = args.join("");  
+    let searchQ = args.join("+");  
     let searchE = ('search?q=' + searchQ + '&sourceid=discord');
-    if(!searchQ) return message.channel.send('Que querias buscar?');
+    if(!searchQ) return message.channel.send('Ingresa lo que deseas buscar');
     const embed = new Discord.RichEmbed()
+        .setThumbnail('https://cdn.pixabay.com/photo/2015/10/31/12/56/google-1015752_960_720.png')
     .setDescription(`Busqueda realizada por **${message.author.username}**`)
     .setTitle('(Google) Buscaste ' + searchQTitle,)
     .addField("https://www.google.com/" + searchE, "Cliquea la página para ver su contenido")
