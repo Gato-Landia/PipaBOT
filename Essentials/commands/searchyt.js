16b27d1c38419edd9d1c15c8fd53c99b8ced6034
KitKat <leonel.-s@hotmail.com>
2019-12-15 22:47:53 -0300

    Update PipaBOT 1.1.4

--- Essentials/commands/searchyt.js
+++ Essentials/commands/searchyt.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 module.exports = {
     name: 'searchyt',
-    aliases: ['search-yt', 'search-youtube', 'searchyoutube'],
+  aliases: ['search-yt', 'search-youtube', 'searchyoutube'],
     execute: async (message, args) => {
     let searchQTitleYT = args.join(" ");
     let searchYTQ = args.join("+");
     let searchYTE = ('results?search_query=' + searchYTQ + '&page=&utm_source=discord');
-    if(!searchYTQ) return message.channel.send('Que querías buscar?');
+    if(!searchYTQ) return message.channel.send('Ingresa lo que deseas buscar');
     const embed = new Discord.RichEmbed()
+          .setThumbnail('https://freepngimg.com/download/youtube/77697-logo-computer-youtube-icons-download-hd-png.png')
     .setDescription(`Busqueda realizada por **${message.author.username}**`)
     .setTitle('(YouTube) Buscaste ' + searchQTitleYT)
     .addField("https://www.youtube.com/" + searchYTE, "Cliquea la página para ver su contenido")
