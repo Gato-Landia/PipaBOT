5886bd723777a088d1ab14319d74db39a5faa009
KitKat <leonel.-s@hotmail.com>
2019-12-15 23:09:25 -0300

    Update PipaBOT 1.1.4

--- Essentials/commands/whatsnew.js
+++ Essentials/commands/whatsnew.js
@@ -9,7 +9,7 @@ module.exports = {
         .setDescription("Versión 1.1.4")
       .addField("Se añadió:", "roll\n searchsp\n whatsnew")
       .addField("Se reparó:", "userinfo")
-      .addField("Se modificó:", "ctof\n ftoc\n searchgl\n ftoc\n searchgl\n searchyt\n suma\n resta\n division\n multiplicacion")
+      .addField("Se modificó:", "ctof\n ftoc\n searchgl\n searchgl\n searchyt\n suma\n resta\n division\n multiplicacion")
       .addField("Se actualizó:", "help\n help2")
       .addField("Se aplicó:", "Esencia de pipa");
       message.channel.send(embed);
